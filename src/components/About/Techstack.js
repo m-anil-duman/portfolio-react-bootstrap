edcec3abca9210d19172eae4740cbbec1a394b3e
m-anil-duman <anil.duman00@gmail.com>
2024-07-18 11:11:11 +0200

    fix

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -8,17 +7,14 @@ import {
   DiMongodb,
   DiPython,
   DiGit,
-  DiJava,
 } from "react-icons/di";
 import {
-  SiRedis,
   SiFirebase,
   SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
   SiRedux,
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+
 
 function Techstack() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by M.Anil Duman</h3>
+          <h3>Developed by M.Anil Duman</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} M.A.D</h3>
