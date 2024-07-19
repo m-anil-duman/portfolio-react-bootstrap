f2f7b0d9651c3915db8f29740b37b196f9edbcf5
m-anil-duman <anil.duman00@gmail.com>
2024-07-19 08:55:06 +0200

    projects responsiveness updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle.js";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import dashboard from "../../Assets/Projects/Dashboard-pj.png";
+import StockTracker from "../../Assets/Projects/StockTracker-cropped.jpg";
+import littleLemon from "../../Assets/Projects/Little Lemon Restaurant Home.png";
 
 function Projects() {
   return (
@@ -21,70 +18,34 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={6} xl={4}className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={StockTracker}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Stock Tracker"
+              description="Stock Tracker app that manages to track live data of stock changes,User can track data in 5 different intervals, add stocks to the favourites to reach it easily. Built with React-Native,Expo Go,GraphQL,Stepzen,PostgreSQL"
+              ghLink="https://github.com/m-anil-duman/StocksPriceTracker"
+              demoLink="https://StockTracker-49.web.app/"
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={6} xl={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={littleLemon}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Little Lemon Restaurant"
+              description="I built a dynamic and fully responsive website for The Little Lemon Restaurant, a Mediterranean restaurant specializing in Italian, Greek, and Turkish cuisine. The site features comprehensive menus and dishes, reservation , with online shopping and login section currently under construction. The project leverages React.js for the frontend and MongoDB for the backend."
+              ghLink="https://github.com/m-anil-duman/little-lemon-restaurant"
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={6} xl={4}className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={dashboard}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="M-[AD]min Dashboard"
+              description="I'm developing a sleek and efficient Admin-Dashboard for my portfolio, utilizing Next.js and MongoDB. This project showcases advanced features such as user management, data analytics, and real-time updates, all within a responsive and intuitive interface. With a focus on performance and scalability, this dashboard demonstrates my ability to create powerful and user-friendly admin tools.Currently under construction"
+              ghLink="https://github.com/m-anil-duman/admin-dashboard-project"
             />
           </Col>
         </Row>
